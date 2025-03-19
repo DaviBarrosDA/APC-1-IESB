74bbbf6e3bc257066aae71ca32a53a98bfdd7141
Davi Barros <davibarros66@gmail.com>
2025-03-19 11:03:09 -0300

    Adicionando arquivos da pratica02

---
+++ aulas/praticas/pratica02/imprima_boletim.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+    int main(){
+        printf("Matricula	A1	A2	Media");
+        printf("20210010"	8.5	5.0	6.4 );
+        printf("");
+        printf("");
+        printf("");
+        printf("");
+        printf("");
+
+        return 0;
+    }
