26c157f341f582864cceb0d206249a7bf3587755
Davi Barros <davibarros66@gmail.com>
2025-04-02 10:53:21 -0300

    codigos das aulas 4 e 5

---
+++ aulas/aula05/operadores_inc_dec.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    // ++ incrementa de 1
+    // -- decrementa de 1
+
+    int numero = 10;
+    printf("Incremento = %i\n", ++numero);
+    printf("Decremento = %i\n", --numero);
+    printf("Pos-Incremento = %i\n", numero++);
+    printf("Pos-decremento = %i\n", numero--);
+
+    
+    return 0;
+}
---
+++ aulas/aula05/operadores_relacionais.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+    int main(){
+        // < menor que
+        // > maior que
+        // <= menor ou iqual
+        // >= maior ou iqual
+        // == igual
+        // != diferente
+        printf("2 eh maior que 5? %i\n", 2 > 5);
+        printf("2 eh maior que 5? %i\n", 2 > 5);
+        printf("2 eh maior que 5? %i\n", 2 > 5);
+        printf("2 eh maior que 5? %i\n", 2 > 5);
+        printf("2 eh maior que 5? %i\n", 2 > 5);
+        printf("2 eh maior que 5? %i\n", 2 > 5);
+
+        return 0;
+    }
