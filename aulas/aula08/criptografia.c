1a046d952ea81e6343e8274ad2aaeb404f211b0f
Davi Barros <davibarros66@gmail.com>
2025-05-28 09:17:49 -0300

    Adicionando pratica 08

---
+++ aulas/aula08/criptografia.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    // atribuir uma string
+    char nome[30] = "Davi Junior";
+    // atribuir um conjunto de caracteres
+    char frase[30] = {'E','u',' ','p','r','o','g','r','a','m','o',' ','e','m',' ','c','\0'};
+
+    for(int i=0; i<17; i++){
+        printf("%c", frase[i]);
+    }
+    printf("\n");
+
+    printf("Entre com uma frase: ");
+    printf("%s", frase);
+    
+    char cesar[6][5];
+    int k=0;
+        for(int i=0; i<6; i++) {
+            for(int j=0; j<5; j++){
+                cesar[i][j] = frase[k];
+                k++;
+        }
+    }
+        printf("A frase codificada: ");
+        for(int j=0;j<5;j++) {
+            for(int i=0;i<6;i++) {
+                printf("%c", cesar[i][j])
+
+            }
+    return 0;
+}
